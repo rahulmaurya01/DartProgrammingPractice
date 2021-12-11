529b416e11d729a7f3a527e00f2faf2121257f29
rahulmaurya01 <rahul9336446070@gmail.com>
2021-12-11 15:03:39 +0530

    wrong_creat

--- bin/dart_application_console.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_application_console/dart_application_console.dart' as dart_application_console;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_console.calculate()}!');
-}
--- test/dart_application_console_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_application_console/dart_application_console.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
